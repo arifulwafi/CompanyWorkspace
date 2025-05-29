628358f9510c9d3598dfd862f93009d44a7306bd
Ariful Islam <aisabuz@gmail.com>
2025-05-29 15:33:14 +0600

    Company Ids checkboxes added on Employee add/edit page

--- src/Cyberjuice.Web/Pages/Employees/CreateModal.cshtml.cs
+++ src/Cyberjuice.Web/Pages/Employees/CreateModal.cshtml.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Cyberjuice.Employees;
 using Cyberjuice.Employees.Dtos;
+using Cyberjuice.Companies;
+using Cyberjuice.Companies.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Volo.Abp.Application.Dtos;
 using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
 
 namespace Cyberjuice.Web.Pages.Employees;
@@ -11,21 +17,37 @@ public class CreateModalModel : AbpPageModel
     [BindProperty]
     public CreateUpdateEmployeeInput Employee { get; set; }
 
-    private readonly IEmployeeAppService _employeeAppService;
+    public List<SelectListItem> Companies { get; set; } = new List<SelectListItem>();
 
-    public CreateModalModel(IEmployeeAppService employeeAppService)
+    private readonly IEmployeeAppService _employeeAppService;
+    private readonly ICompanyAppService _companyAppService;
+
+    public CreateModalModel(IEmployeeAppService employeeAppService, ICompanyAppService companyAppService)
     {
         _employeeAppService = employeeAppService;
+        _companyAppService = companyAppService;
     }
 
-    public void OnGet()
+    public async Task OnGetAsync()
     {
         Employee = new CreateUpdateEmployeeInput();
+        await LoadCompaniesAsync();
     }
 
     public async Task<IActionResult> OnPostAsync()
     {
         await _employeeAppService.CreateAsync(Employee);
         return NoContent();
+    }
+
+    private async Task LoadCompaniesAsync()
+    {
+        var companies = await _companyAppService.GetAllAsync(new PagedAndSortedResultRequestDto { MaxResultCount = 1000 });
+        
+        Companies = companies.Items.Select(c => new SelectListItem
+        {
+            Value = c.Id.ToString(),
+            Text = c.Name
+        }).ToList();
     }
 } 
--- src/Cyberjuice.Web/Pages/Employees/EditModal.cshtml.cs
+++ src/Cyberjuice.Web/Pages/Employees/EditModal.cshtml.cs
@@ -1,8 +1,14 @@
 using Cyberjuice.Employees;
 using Cyberjuice.Employees.Dtos;
+using Cyberjuice.Companies;
+using Cyberjuice.Companies.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
 using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
 
 namespace Cyberjuice.Web.Pages.Employees;
@@ -17,12 +23,15 @@ public class EditModalModel : AbpPageModel
     public CreateUpdateEmployeeInput Employee { get; set; }
     
     public int RemainingLeaveDays { get; set; }
+    public List<SelectListItem> Companies { get; set; } = new List<SelectListItem>();
 
     private readonly IEmployeeAppService _employeeAppService;
+    private readonly ICompanyAppService _companyAppService;
 
-    public EditModalModel(IEmployeeAppService employeeAppService)
+    public EditModalModel(IEmployeeAppService employeeAppService, ICompanyAppService companyAppService)
     {
         _employeeAppService = employeeAppService;
+        _companyAppService = companyAppService;
     }
 
     public async Task OnGetAsync()
@@ -37,15 +46,28 @@ public class EditModalModel : AbpPageModel
             PhoneNumber = employee.PhoneNumber,
             DateOfBirth = employee.DateOfBirth,
             JoiningDate = employee.JoiningDate,
-            TotalLeaveDays = employee.TotalLeaveDays
+            TotalLeaveDays = employee.TotalLeaveDays,
+            CompanyIds = employee.CompanyIds
         };
         
         RemainingLeaveDays = employee.RemainingLeaveDays;
+        await LoadCompaniesAsync();
     }
 
     public async Task<IActionResult> OnPostAsync()
     {
         await _employeeAppService.UpdateAsync(Id, Employee);
         return NoContent();
+    }
+
+    private async Task LoadCompaniesAsync()
+    {
+        var companies = await _companyAppService.GetAllAsync(new PagedAndSortedResultRequestDto { MaxResultCount = 1000 });
+        
+        Companies = companies.Items.Select(c => new SelectListItem
+        {
+            Value = c.Id.ToString(),
+            Text = c.Name
+        }).ToList();
     }
 } 
